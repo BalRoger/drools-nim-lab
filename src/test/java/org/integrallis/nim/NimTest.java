daceabd76b63f20f091a15102980d93afe02adfa
Brian Sam-Bodden <bsbodden@integrallis.com>
2017-06-26 07:12:04 -0700

    Upgrade to Drools 7.0.0.final and Maven

--- src/test/java/org/integrallis/nim/NimTest.java
+++ src/test/java/org/integrallis/nim/NimTest.java
@@ -1,11 +1,5 @@
 package org.integrallis.nim;
 
-import static org.integrallis.nim.Move.*;
-import static org.integrallis.nim.Outcome.OutcomeType.*;
-import static org.junit.Assert.assertTrue;
-
-import org.kie.api.runtime.rule.QueryResults;
-import org.kie.api.runtime.rule.QueryResultsRow;
 import org.integrallis.drools.junit.BaseDroolsTestCase;
 import org.junit.Before;
 import org.junit.Test;
